eb431b511384eb891be63f0e01c36fd8748e884b
Izhar <izhar@script-rocket.com>
2022-01-27 18:03:18 -0600

    added webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+// import 'dart:ffi';
+// import 'dart:js';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,105 +18,183 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter xx ',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // intial loaded URL
+final webViewReload = GlobalKey<ChangeRouteState>();
+String routeUrl = 'https://www.socialbite.co/login';
+String newURL = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // void _buttonPree(BuildContext context, String newURL){
+  //   Navigator.push(context,
+  //     MaterialPageRoute( builder: (context) => ChangeRoute(newURL)));
+  // }
+
+  void routeChanger() {
+    print("routeURL Now: $newURL");
+    webViewReload.currentState?.reloadPage(newURL);
+    print("routeURL $routeUrl");
+    // setState(() {
+    //   print("route URL: $routeUrl");
+
+    //   // Navigator.push(
+    //   //     context,
+    //   //     MaterialPageRoute(
+    //   //         builder: (BuildContext context) =>
+    //   //             ChangeRoute(routeUrl: routeUrl,)));
+    // });
   }
+
+  // TODO make all strings a final variable
+  // TODO move styles to styles file
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 50,
+          iconTheme: IconThemeData(color: Colors.black54),
+          backgroundColor: Colors.white70,
+          shadowColor: Colors.grey[50],
+          centerTitle: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                // TODO change image to SVG
+                'assets/images/logo.png',
+                fit: BoxFit.contain,
+                height: 32,
+              )
+            ],
+          ),
+        ),
+        endDrawer: Drawer(
+          backgroundColor: Colors.blueGrey[50],
+          child: Container(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    webViewReload.currentState?.reloadPage('https://www.socialbite.co');
+                  },
+                  icon: Icon(Icons.account_circle),
+                  label: Text("Profile"),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    newURL = 'https://www.socialbite.co';
+                    routeChanger();
+                  },
+                  icon: Icon(Icons.settings),
+                  label: Text("Settings"),
+                ),
+                TextButton.icon(
+                  onPressed: null,
+                  icon: Icon(Icons.help_rounded),
+                  label: Text("Support"),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: const ChangeRoute(
+            // routeUrl: routeUrl,
+            ));
+  }
+}
+
+// JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+//   return JavascriptChannel(
+//       name: 'Toaster',
+//       onMessageReceived: (JavascriptMessage message) {
+//         // ignore: deprecated_member_use
+//         Scaffold.of(context).showSnackBar(
+//           SnackBar(content: Text(message.message)),
+//         );
+//       });
+// }
+
+class ChangeRoute extends StatefulWidget {
+  const ChangeRoute({Key? key}) : super(key: key);
+
+  @override
+  ChangeRouteState createState() => ChangeRouteState();
+}
+
+class ChangeRouteState extends State<ChangeRoute> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  late WebViewController _webViewController;
+
+  // final Function() refresh;
+  String routeUrl = 'https://www.socialbite.co/login';
+  // ChangeRoute({Key? key, required this.refresh}) : super(key: key);
+  // ChangeRoute({Key? key, required this.routeUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return WebView(
+      zoomEnabled: false,
+      initialUrl: routeUrl,
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _webViewController = webViewController;
+        _controller.complete(webViewController);
+      },
+      onProgress: (int progress) {
+        print("webview is loading: $progress%");
+      },
+      // javascriptChannels: <JavascriptChannel>{
+      //   _toasterJavascriptChannel(context),
+      // },
+      // navigationDelegate: (NavigationRequest request) {
+      //   if (request.url.contains('socialbite.co/user')) {
+      //     return NavigationDecision.navigate;
+      //   }
+      //   return NavigationDecision.prevent;
+      // },
+      onPageStarted: (String url) {
+        print('Page started loading: $url');
+      },
+      onPageFinished: (String url) {
+        print('Page finished loading: $url');
+        _webViewController
+            .runJavascriptReturningResult("javascript:(function() { " +
+                "var head = document.getElementsByTagName('header')[0];" +
+                "head.parentNode.removeChild(head);" +
+                "var footer = document.getElementsByTagName('footer')[0];" +
+                "footer.parentNode.removeChild(footer);" +
+                "})()")
+            .then((value) => debugPrint('Page finished loading Javascript'))
+            .catchError((onError) => debugPrint('$onError'));
+      },
+      gestureNavigationEnabled: true,
     );
+  }
+
+  void reloadPage( String newURL) {
+    print("reload");
+    setState(() {
+      routeUrl = newURL;
+    });
+    _webViewController.reload();
   }
 }
